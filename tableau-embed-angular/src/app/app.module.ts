90b4ade385eb71982b5f1fb51dbfec9629ce6274
Takashi Binns <takashi.binns@icloud.com>
2022-04-19 17:17:57 -0400

    making it look nicer

--- tableau-embed-angular/src/app/app.module.ts
+++ tableau-embed-angular/src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableauEmbededVizComponent } from './tableau-embeded-viz/tableau-embeded-viz.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//  Material Design modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -11,7 +19,14 @@ import { TableauEmbededVizComponent } from './tableau-embeded-viz/tableau-embede
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatBadgeModule,
+    MatProgressSpinnerModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- tableau-embed-angular/src/app/tableau-embeded-viz/tableau-embeded-viz.component.ts
+++ tableau-embed-angular/src/app/tableau-embeded-viz/tableau-embeded-viz.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 
+
+
+const bufferSize = 25;
 @Component({
   selector: 'app-tableau-embeded-viz',
   templateUrl: './tableau-embeded-viz.component.html',
@@ -9,9 +12,42 @@ export class TableauEmbededVizComponent implements OnInit {
 
   constructor() { }
 
-  @Input() vizUrl = ''; //  Inherit from the top level component
-  @Input() divId = ''; //  Inherit from the top level component
-
+  //  Inherit attributes from the parent component
+  @Input() vizUrl = ''; 
+  @Input() divId = ''; 
+  @Input() workbookOwner = '';
+  @Input() workbookDescription = '';
+  
+  //  Make sure the dashboard is as wide as the user's screen, and uses a 4:3 aspect ratio
+  public getScreenWidth: any;
+  public getScreenHeight: any;
   ngOnInit(): void {
+    this.getScreenWidth = window.innerWidth-bufferSize;
+    this.getScreenHeight = (window.innerWidth-bufferSize)*3/4;
+  }
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth-bufferSize;
+    this.getScreenHeight = (window.innerWidth-bufferSize)*3/4;
+  }
+
+  //  Get information about the dashboard
+  public dashboardName = '';
+  public thisViz: any;
+  ngAfterViewInit() {
+    //  Store a reference to this
+    let thisComponent = this;
+    //  Define a function that can be run, on
+    let viz = document.getElementById(this.divId);
+    if (viz) {
+      //  Add event handler to trigger once the viz is finished loading
+      viz.addEventListener("firstinteractive", async (event) => {
+        //  Need to cast the HtmlElement to an object (for TypeScript)
+        thisComponent.thisViz = <any>viz;
+        //  Update the name and attributes of this dashboard
+        thisComponent.dashboardName = thisComponent.thisViz.workbook.activeSheet.name;
+      });
+    }
+
   }
 }
